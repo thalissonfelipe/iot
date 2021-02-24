005553b1eef59d3674bebaabb610e858ad675f74
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-02-24 19:52:41 -0300

    add new topics on publisher

--- backend/python/publisher.py
+++ backend/python/publisher.py
@@ -15,36 +15,35 @@ def fahrenheit_to_celsius(temp):
     return (temp - 32) / 1.8
 
 
-df = pd.read_csv('reduced_dataset.csv', sep=',')
-df['inTemp'] = df['inTemp'].apply(fahrenheit_to_celsius)
-df['outTemp'] = df['outTemp'].apply(fahrenheit_to_celsius)
+df = pd.read_csv('recipeful_fridge_dataset.csv', sep=',')
+df['Temperature_A'] = df['Temperature_A'].apply(fahrenheit_to_celsius)
+df['Temperature_B'] = df['Temperature_B'].apply(fahrenheit_to_celsius)
+df['Temperature_C'] = df['Temperature_C'].apply(fahrenheit_to_celsius)
 
 index = 0
 
-# atualizar valor de peso a cada 120k ms
+priority = {
+    'Baixa': 0,
+    'Média': 1,
+    'Media': 1,
+    'Alta': 2
+}
 
-def get_random_value(channel):
+def get_random_value(recipient, channel):
     global index
-    value = 0
-    if channel == 1:
-        value = df['inTemp'].values[index]
-    elif channel == 2:
-        value = df['inHum'].values[index]
-    elif channel == 3:
-        value = random.uniform(0, 50)
-
+    value = df[f'{channel}_{recipient}'].values[index]
     index += 1
-    if index == len(df['inTemp'].values):
+    if index == len(df):
         index = 0
 
-    return round(value, 2)
+    return round(value, 2) if isinstance(value, float) else priority[value]
 
 
 def on_connect(client, userdata, _flags, rc):
     print('Connected with cayenne broker!')
 
 
-def create_recipient_client(client_id):
+def create_recipient_client(client_id, recipient):
     client = mqtt.Client(client_id)
     client.username_pw_set(config['mqtt']['username'], config['mqtt']['password'])
     client.connect(config['mqtt']['address'], config['mqtt']['port'], keepalive=60)
@@ -52,15 +51,21 @@ def create_recipient_client(client_id):
 
     temperature_topic = f'v1/{config["mqtt"]["username"]}/things/{client_id}/data/1'
     humidity_topic = f'v1/{config["mqtt"]["username"]}/things/{client_id}/data/2'
-    weight_topic = f'v1/{config["mqtt"]["username"]}/things/{client_id}/data/3'
+    weight_topic1 = f'v1/{config["mqtt"]["username"]}/things/{client_id}/data/3'
+    weight_topic2 = f'v1/{config["mqtt"]["username"]}/things/{client_id}/data/4'
+    weight_topic3 = f'v1/{config["mqtt"]["username"]}/things/{client_id}/data/5'
+    priority_topic = f'v1/{config["mqtt"]["username"]}/things/{client_id}/data/6'
 
     time.sleep(1)
     client.loop_start()
 
     while True:
-        client.publish(temperature_topic, get_random_value(1))
-        client.publish(humidity_topic, get_random_value(2))
-        client.publish(weight_topic, get_random_value(3))
+        client.publish(temperature_topic, get_random_value(recipient, 'Temperature'))
+        client.publish(humidity_topic, get_random_value(recipient, 'Humidity'))
+        client.publish(weight_topic1, get_random_value(recipient+'1', 'Weight'))
+        client.publish(weight_topic2, get_random_value(recipient+'2', 'Weight'))
+        client.publish(weight_topic3, get_random_value(recipient+'3', 'Weight'))
+        client.publish(priority_topic, get_random_value(recipient, 'Priority'))
 
         time.sleep(3)
 
@@ -69,5 +74,5 @@ def create_recipient_client(client_id):
 
 
 if __name__ == '__main__':
-    for id in config['mqtt']['recipients']:
-        Thread(target=create_recipient_client, args=[id]).start()
+    for id, recipient in zip(config['mqtt']['recipients'], ['A', 'B', 'C']):
+        Thread(target=create_recipient_client, args=[id, recipient]).start()
