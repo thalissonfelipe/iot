9808d506262a4409d00927ba63735742511cd84c
Thalisson Sousa <thalisson.sousa@lsbd.ufc.br>
2021-02-23 21:19:22 -0300

    simulating sensors from fridge dataset

--- backend/python/publisher.py
+++ backend/python/publisher.py
@@ -2,37 +2,55 @@ import os
 import time
 import json
 import random
+import pandas as pd
+from threading import Thread
 import paho.mqtt.client as mqtt
 
 
 with open(os.path.join('..', 'config.json')) as json_file:
-    json_data = json.load(json_file)
+    config = json.load(json_file)
 
+
+def fahrenheit_to_celsius(temp):
+    return (temp - 32) / 1.8
+
+
+df = pd.read_csv('reduced_dataset.csv', sep=',')
+df['inTemp'] = df['inTemp'].apply(fahrenheit_to_celsius)
+df['outTemp'] = df['outTemp'].apply(fahrenheit_to_celsius)
+
+index = 0
 
 def get_random_value(channel):
+    global index
     value = 0
     if channel == 1:
-        value = random.uniform(-10, 10)
+        value = df['inTemp'].values[index]
     elif channel == 2:
-        value = random.uniform(-10, 10)
+        value = df['inHum'].values[index]
     elif channel == 3:
-        value = random.uniform(-10, 10)
+        value = random.uniform(100, 500)
+
+    index += 1
+    if index == len(df['inTemp'].values):
+        index = 0
+
     return round(value, 2)
 
 
 def on_connect(client, userdata, _flags, rc):
-    print('Connected')
+    print('Connected with cayenne broker!')
 
 
 def create_recipient_client(client_id):
     client = mqtt.Client(client_id)
-    client.username_pw_set(json_data['mqtt']['username'], json_data['mqtt']['password'])
-    client.connect(json_data['mqtt']['address'], json_data['mqtt']['port'], keepalive=60)
+    client.username_pw_set(config['mqtt']['username'], config['mqtt']['password'])
+    client.connect(config['mqtt']['address'], config['mqtt']['port'], keepalive=60)
     client.on_connect = on_connect
 
-    temperature_topic = f'v1/{json_data["mqtt"]["username"]}/things/{client_id}/data/1'
-    humidity_topic = f'v1/{json_data["mqtt"]["username"]}/things/{client_id}/data/2'
-    weight_topic = f'v1/{json_data["mqtt"]["username"]}/things/{client_id}/data/3'
+    temperature_topic = f'v1/{config["mqtt"]["username"]}/things/{client_id}/data/1'
+    humidity_topic = f'v1/{config["mqtt"]["username"]}/things/{client_id}/data/2'
+    weight_topic = f'v1/{config["mqtt"]["username"]}/things/{client_id}/data/3'
 
     time.sleep(1)
     client.loop_start()
@@ -49,5 +67,5 @@ def create_recipient_client(client_id):
 
 
 if __name__ == '__main__':
-    for id in json_data['mqtt']['recipients']:
-        create_recipient_client(id)
+    for id in config['mqtt']['recipients']:
+        Thread(target=create_recipient_client, args=[id]).start()
